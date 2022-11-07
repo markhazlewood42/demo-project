fc094550e25c06a7e92fb0f9af16c64d4673ad44
Mark Hazlewood <mhazlewood@hubspot.com>
2022-11-07 09:26:23 -0500

    Added SF to echo query params

--- src/app/app.functions/echoParams.js
+++ src/app/app.functions/echoParams.js
@@ -1,6 +1,6 @@
 exports.main = async (context, callback) => {
     callback({
         statusCode: 200,
-        body: { message: JSON.stringify(context) }
+        body: { message: JSON.stringify(context.params) }
     })
 }
